efb38a0d23731da2298fa507f8a399a070cc173b
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-26 21:50:40 +0530

    Create program08.c

---
+++ program08.c
@@ -0,0 +1,18 @@
+/**
+* program08.c
+* Reading and Printing Basic Data Type - float
+*/
+
+#include<stdio.h>
+
+int main()
+{
+	float singlePrecision;
+	
+	printf("Enter the Single Precision Value : ");
+	scanf("%f", &singlePrecision);
+
+    printf("User Input Value : %f.", singlePrecision);
+
+    return 0;
+}
