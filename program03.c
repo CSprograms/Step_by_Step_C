f4035fd9446ca7d268c2b152a92a468cbf94ce91
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-26 21:50:02 +0530

    Create program03.c

---
+++ program03.c
@@ -0,0 +1,15 @@
+/**
+* program03.c
+* Printing Basic Data Type - int
+*/
+
+#include<stdio.h>
+
+int main()
+{
+	int integer = 24;
+
+    printf("Printing Integer Value : %d.", integer);
+	
+	return 0;
+}
