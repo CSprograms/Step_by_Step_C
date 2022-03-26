fd4ce58d73b633ddec54396019d88da3fe408113
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-26 21:51:02 +0530

    Create program10.c

---
+++ program10.c
@@ -0,0 +1,18 @@
+/**
+* program10.c
+* Reading and Printing Basic Data Type - double
+*/
+
+#include<stdio.h>
+
+int main()
+{
+	double doublePrecision;
+	
+	printf("Enter the Double Precision Value : ");
+	scanf("%lf", &doublePrecision);
+
+    printf("Printing Double ( Double Precision ) Value : %lf.", doublePrecision);
+	
+    return 0;
+}
