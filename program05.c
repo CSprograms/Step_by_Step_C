06575e7a4c5bd6d6c10fae5d750a1b86626f8a63
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-26 21:50:16 +0530

    Create program05.c

---
+++ program05.c
@@ -0,0 +1,15 @@
+/**
+* program05.c
+* Printing Basic Data Type - char
+*/
+
+#include<stdio.h>
+
+int main()
+{
+	char character = 'A';
+
+    printf("Printing Character Value : %c.", character);
+
+    return 0;
+}
