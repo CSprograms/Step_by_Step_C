affce8a0a4620a4db58e810965eb9127753aa1e8
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-26 21:50:55 +0530

    Create program09.c

---
+++ program09.c
@@ -0,0 +1,15 @@
+/**
+* program09.c
+* Printing Basic Data Type - double
+*/
+
+#include<stdio.h>
+
+int main()
+{
+	double doublePrecision = 24.246;
+
+    printf("Printing Double ( Double Precision ) Value : %lf.", doublePrecision);
+	
+    return 0;
+}
