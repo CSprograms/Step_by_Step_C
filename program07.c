1388ec222c46a40d8544307d87a532449c702ad2
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-26 21:50:28 +0530

    Create program07.c

---
+++ program07.c
@@ -0,0 +1,15 @@
+/**
+* program07.c
+* Printing Basic Data Type - float
+*/
+
+#include<stdio.h>
+
+int main()
+{
+	float singlePrecision = 24.246;
+
+    printf("Printing Float ( Single Precision ) Value : %f.", singlePrecision);
+	
+    return 0;
+}
