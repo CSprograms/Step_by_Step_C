6d6d7e8c1eaf5f22263831e00ea59835ccc87150
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-26 21:50:11 +0530

    Create program04.c

---
+++ program04.c
@@ -0,0 +1,18 @@
+/**
+* program04.c
+* Reading and Printing Basic Data Type - int
+*/
+
+#include<stdio.h>
+
+int main()
+{
+	int integer;
+	
+	printf("Enter the Integer Value : ");
+	scanf("%d", &integer);
+
+    printf("User Input Value : %d.", integer);
+
+    return 0;
+}
